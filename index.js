1129ebc416a428b39d341468382e895af4291906
tunnckoCore <mameto_100@mail.bg>
2015-01-20 04:32:44 +0200

    add support for glob filters using `micromatch`

--- index.js
+++ index.js
@@ -5,12 +5,22 @@
  * Licensed under the MIT license.
  */
 
+var mm = require('micromatch');
 var forOwn = require('for-own');
 var iterator = require('make-iterator');
 
-module.exports = function filterValues(obj, cb, thisArg) {
-  cb = iterator(cb, thisArg);
+/**
+ * Filter an object values using glob patterns
+ * or with a `callback` function returns true.
+ *
+ * @param  {Object} `obj`
+ * @param  {Function|Array|String} `filter`
+ * @param  {Object} `thisArg`
+ * @return {Object}
+ */
+module.exports = function filterValues(obj, filter, thisArg) {
   var res = {};
+  var cb = makeIterator(filter, thisArg);
 
   forOwn(obj, function (val, key, o) {
     if (cb(val, key, o)) {
@@ -19,3 +29,12 @@ module.exports = function filterValues(obj, cb, thisArg) {
   });
   return res;
 };
+
+function makeIterator(filter, thisArg) {
+  if (Array.isArray(filter) || typeof filter === 'string') {
+    return function (val) {
+      return mm(val, filter).length ? true : false;
+    };
+  }
+  return iterator(filter, thisArg)
+}
