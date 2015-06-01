1ff8e14978165039ec8d9376f586f7c9166c0949
tunnckoCore <mameto_100@mail.bg>
2015-06-01 07:18:41 +0300

    fix tests

--- test.js
+++ test.js
@@ -13,22 +13,22 @@ var filter = require('./');
 
 describe('.filter()', function () {
   it('should return values for which the callback returns true.', function () {
-    var o = filter({a: 'a', b: 'b', c: 'c'}, function (value, key) {
-      return key === 'b';
+    var o = filter({a: 'a', b: 'b', c: 'c'}, function (value) {
+      return value === 'b';
     });
     o.should.eql({ b: 'b' });
   });
 
   it('should omit values for which the callback returns false.', function () {
-    var o = filter({a: 'a', b: 'b', c: 'c'}, function (value, key) {
-      return key !== 'b';
+    var o = filter({a: 'a', b: 'b', c: 'c'}, function (value) {
+      return value !== 'b';
     });
     o.should.eql({a: 'a', c: 'c'});
   });
 
   it('should return an empty object if all values return false.', function () {
-    var o = filter({a: 'a', b: 'b', c: 'c'}, function (value, key) {
-      return key === 'foo';
+    var o = filter({a: 'a', b: 'b', c: 'c'}, function (value) {
+      return value === 'foo';
     });
     o.should.eql({});
   });
